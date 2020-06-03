fbfc95159b041967a06e6217153a66c60ca19a11
Ahmed Raza Mustafa <45358221+AhmedRazaPeruse@users.noreply.github.com>
2020-06-04 04:08:59 +0500

    Add bootcamp-react0604 files

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to Bootcamp 2020 React Application Development!
         </p>
         <a
           className="App-link"
